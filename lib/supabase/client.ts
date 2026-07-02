9664bceecbb328aaa59b285a590917a145fa940d
jusitamd <jusitamd999@gmail.com>
2026-07-02 14:23:32 +0700

    dùng key Supabase thật để đăng nhập hoạt động

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://dummy.supabase.co",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "dummy"
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://pefqwttyxumdzyhgpwyx.supabase.co",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlZnF3dHR5eHVtZHp5aGdwd3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NDQxMDIsImV4cCI6MjA5ODMyMDEwMn0.GErXjYDP0i18F-TDWsSOd7ygsvBAGeS-sjK-h37UEIw"
   )
 }
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -5,8 +5,8 @@ export function createClient() {
   const cookieStore = cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://dummy.supabase.co",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "dummy",
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "https://pefqwttyxumdzyhgpwyx.supabase.co",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlZnF3dHR5eHVtZHp5aGdwd3l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3NDQxMDIsImV4cCI6MjA5ODMyMDEwMn0.GErXjYDP0i18F-TDWsSOd7ygsvBAGeS-sjK-h37UEIw",
     {
       cookies: {
         get(name: string) {
